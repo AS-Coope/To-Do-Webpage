5b13116ff6f4b6715ec2135b9e7398666abdc9cb
AS-Coope <deltazirconium@gmail.com>
2023-11-07 17:08:23 -0500

    Create functions that perform operations to create and add a todo to the list

--- js/main.js
+++ js/main.js
@@ -7,6 +7,38 @@ function main() {
     let addToDoBtn = document.getElementsByClassName("add-todo")[0];
 
     addToDoBtn.addEventListener('click', () => {
+
+        function createElementWithClass(element, className) {
+            let elemWithClass = document.createElement(element);
+            elemWithClass.setAttribute("class", className);
+        }
+
+        function createAndReturnSingleToDo() {
+            elemWithClass = createElementWithClass("div", "single-todo");
+            elemWithClass.appendChild(createElementWithClass("div", "todo-text"));
+            elemWithClass.appendChild(createElementWithClass("div", "delete-button"));
+            return elemWithClass;
+        }
+
+        function getToDoTextElement(singleToDo) {
+            return singleToDo.querySelector(".todo-text");
+        }
+
+        function setTextContentOfToDoTextElement(toDoTextElem, userTextContent) {
+            toDoTextElem.textContent = userTextContent;
+        }
+
+        function addSingleToDoToContainer(tdlContainer, theSingleToDo) {
+            tdlContainer.appendChild(theSingleToDo);
+        }
+
+        function createToDo(userToDoInput) {
+            let singleToDo = createAndReturnSingleToDo();
+            let toDoText = getToDoTextElement();
+            setTextContentOfToDoTextElement(toDoText, userToDoInput);
+            addSingleToDoToContainer(toDoListContainer, singleToDo);
+        }
+
         let myDiv = document.createElement("h2");
         let toDo = prompt("Write a ToDo");
         if (toDo !== null && toDo !== "") {
