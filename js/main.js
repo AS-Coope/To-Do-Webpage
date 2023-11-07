9d13afa12b452279747a79c277ebdd7f19d70ebc
AS-Coope <deltazirconium@gmail.com>
2023-11-07 13:56:37 -0500

    Test basic working of ToDo

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    main();
+});
+
+function main() {
+    let toDoListContainer = document.getElementsByClassName("container")[0];
+
+    /*
+    toDoListContainer.addEventListener('click', () => {
+        let myDiv = document.createElement("h2");
+        let toDo = prompt("Write a ToDo");
+        if (toDo !== null) {
+            myDiv.textContent = toDo;
+            toDoListContainer.appendChild(myDiv);
+        } else {
+            alert("No ToDo entered!");
+            console.log(toDo);
+        }
+    });
+    */
+}
