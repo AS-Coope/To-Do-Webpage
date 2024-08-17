ceb74101d2f0524c88ddce9d45fcddb1de2de6e8
AS-Coope <deltazirconium@gmail.com>
2024-08-17 17:17:46 -0500

    Remove duplicate functions now moved to ToDo class

--- js/main.js
+++ js/main.js
@@ -5,37 +5,6 @@ let todoList = [];
 
 addToDoBtn.addEventListener('click', () => {
 
-    function createElementWithClass(element, className) {
-        let elemWithClass = document.createElement(element);
-        elemWithClass.setAttribute("class", className);
-        return elemWithClass;
-    }
-
-    function createAndReturnSingleToDo() {
-        elemWithClass = createElementWithClass("div", "single-todo");
-        elemWithClass.appendChild(createElementWithClass("div", "todo-text"));
-        elemWithClass.appendChild(createElementWithClass("div", "delete-button")).textContent = "Delete";
-        return elemWithClass;
-    }
-
-    function getToDoTextElement(singleToDo) {
-        return singleToDo.querySelector(".todo-text");
-    }
-
-    function getToDoDeleteBtnElement(singleToDo) {
-        return singleToDo.querySelector(".delete-button");
-    }
-
-    function addDeleteEventListener(delBtn) {
-        delBtn.addEventListener('click', () => {
-            delBtn.parentNode.remove();
-        })
-    }
-
-    function setTextContentOfToDoTextElement(toDoTextElem, userTextContent) {
-        toDoTextElem.textContent = userTextContent;
-    }
-
     function addSingleToDoToContainer(tdlContainer, theSingleToDo) {
         tdlContainer.appendChild(theSingleToDo);
     }
