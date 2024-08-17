8a41ef0e52124833282cbad0c9c232ebf9024372
AS-Coope <deltazirconium@gmail.com>
2024-08-17 17:16:33 -0500

    Create ToDo class

--- js/main.js
+++ js/main.js
@@ -59,3 +59,71 @@ addToDoBtn.addEventListener('click', () => {
         console.log(userInput, typeof userInput);
     }
 });
+
+class Todo {
+
+    static classId = 0;
+    todo_text;
+    todo_body;
+
+    // text, id, order
+    todo_values = {
+        text: "",
+        id: 0,
+        order: 0
+    }
+    constructor() {
+        this.todo_values.id = Todo.classId;
+        Todo.classId += 1;
+    }
+
+    createElementWithClass(element, className) {
+        let elemWithClass = document.createElement(element);
+        elemWithClass.setAttribute("class", className);
+        return elemWithClass;
+    }
+
+    // sets up the HTML Element that represents the ToDo that will be placed in the DOM
+    createAndReturnSingleToDo() {
+        let elemWithClass = this.createElementWithClass("div", "single-todo");
+        elemWithClass.appendChild(this.createElementWithClass("div", "todo-text"));
+        elemWithClass.appendChild(this.createElementWithClass("button", "delete-button")).textContent = "Delete";
+        this.todo_body = elemWithClass;
+    }
+
+    getToDoBody() {
+        return this.todo_body;
+    }
+
+    getToDoTextElement() {
+        return this.getToDoBody().querySelector(".todo-text");
+    }
+
+    getToDoDeleteBtnElement() {
+        return this.getToDoBody().querySelector(".delete-button");
+    }
+
+    addDeleteEventListener() {
+        console.log(`@ creating delete button: ${todoList}`);
+        this.getToDoDeleteBtnElement().addEventListener('click', () => {
+            // this will also need code to remove the todo from local storage
+            // find the index of current todo.todo_values
+            // then remove it from the list
+            let idx = todoList.findIndex(values => values.id === this.todo_values.id);
+            todoList.splice(idx, 1);
+            this.getToDoDeleteBtnElement().parentNode.remove();
+        })
+    }
+
+    setTextContentOfToDoTextElement(userTextContent) {
+        this.getToDoTextElement().textContent = userTextContent;
+        this.todo_values.text = userTextContent
+    }
+
+    addSingleToDoToContainer(tdlContainer, theSingleToDo) {
+        tdlContainer.appendChild(theSingleToDo);
+    }
+}
+
+// when the delete button on a todo is clicked
+// that todo should be removed from the todoList
